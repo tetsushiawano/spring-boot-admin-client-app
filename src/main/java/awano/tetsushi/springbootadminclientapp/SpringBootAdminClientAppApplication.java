99fa99fa52b5eeb910c08d290d029fc961a0717e
Tetsushi Awano <tetsushi.awano@gmail.com>
2018-02-18 21:50:19 +0900

    Initial Commit.

---
+++ src/main/java/awano/tetsushi/springbootadminclientapp/SpringBootAdminClientAppApplication.java
@@ -0,0 +1,12 @@
+package awano.tetsushi.springbootadminclientapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminClientAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminClientAppApplication.class, args);
+	}
+}
---
+++ src/test/java/awano/tetsushi/springbootadminclientapp/SpringBootAdminClientAppApplicationTests.java
@@ -0,0 +1,16 @@
+package awano.tetsushi.springbootadminclientapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBootAdminClientAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
